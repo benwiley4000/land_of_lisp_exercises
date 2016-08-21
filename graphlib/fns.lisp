5f0807483128dd05ee95e18f4a13af720998f7e0
Ben Wiley <therealbenwiley@gmail.com>
2016-08-20 22:14:58 -0400

    Undirected graph library functions added; undirected graph data and png generated for wizard adventure.

--- graphlib/fns.lisp
+++ graphlib/fns.lisp
@@ -31,10 +31,30 @@
                (cdr node)))
     edges))
 
+(defun uedges->dot (edges)
+    (maplist (lambda (lst)
+                 (mapc (lambda (edge)
+                           (unless (assoc (car edge) (cdr lst))
+                               (fresh-line)
+                               (princ (dot-name (caar lst)))
+                               (princ "--")
+                               (princ (dot-name (car edge)))
+                               (princ "[label=\"")
+                               (princ (dot-label (cdr edge)))
+                               (princ "\"];")))
+                       (cdar lst)))
+             edges))
+
 (defun graph->dot (nodes edges)
     (princ "digraph{")
     (nodes->dot nodes)
     (edges->dot edges)
+    (princ "}"))
+
+(defun ugraph->dot (nodes edges)
+    (princ "graph{")
+    (nodes->dot nodes)
+    (uedges->dot edges)
     (princ "}"))
 
 (defun dot->png (fname thunk)
@@ -49,3 +69,8 @@
     (dot->png fname
               (lambda ()
                   (graph->dot nodes edges))))
+
+(defun ugraph->png (fname nodes edges)
+    (dot->png fname
+              (lambda ()
+                  (ugraph->dot nodes edges))))
